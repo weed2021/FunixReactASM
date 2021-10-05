de925a2675f0758c402ffcfc85ef2c9d9b992d79
weed2021 <quanlbfx12493@funix.edu.vn>
2021-10-05 11:35:39 +0700

    Add tinh nang click va render ra thong tin staff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React,{Component} from 'react';
 import {Navbar, NavbarBrand} from "reactstrap";
 import './App.css';
 import StaffList from './components/StaffListComponent';
-import {STAFFS} from './shared/staffs'
+import {STAFFS,DEPARTMENTS} from './shared/staffs'
+
 class App extends Component{
   constructor(props){
     super(props);
     this.state={
-      staffs:STAFFS
+      staffs:STAFFS,
+      departments:DEPARTMENTS
     };
   }
   render(){
@@ -18,7 +20,7 @@ class App extends Component{
               <NavbarBrand>HR Managerment App</NavbarBrand>
           </div>
         </Navbar>
-        <StaffList staffs={this.state.staffs} />
+        <StaffList departments={this.state.departments} staffs={this.state.staffs} />
       </div>
     );
   }
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,18 +1,58 @@
 import React,{Component} from "react";
-import {Button} from "reactstrap";
+import {Card, Button, CardHeader, CardFooter, CardBody,
+    CardTitle, CardText} from "reactstrap";
+import dateFormat from "dateformat";
+
 class StaffList extends Component{
     constructor(props){
         super(props);
         this.state = {
+            selectedStaff: null
             
         }
     }
+
+    onStaffSelect(staff){
+        this.setState({selectedStaff:staff})
+    }
+
+    renderStaff(staff,DEPARTMENTS){
+        // const DEPARTMENTS = DEPARTMENTS;
+        if(staff!=null){
+            return(
+                <Card>
+                    <CardHeader tag="h6">{staff.name}</CardHeader>
+                    <CardBody>
+                        <CardTitle >Phòng Ban:{' '}{staff.department.name}</CardTitle>
+                        <CardText> Ngày sinh: {' '}
+                            {dateFormat(staff.doB,"paddedShortDate")}
+                        </CardText>
+                        <CardText> Ngày vào công ty: {' '}
+                            {dateFormat(staff.startDate,"paddedShortDate")}
+                        </CardText>
+                        <CardText> Ngày nghỉ còn lại: {' '}
+                            {staff.annualLeave}
+                        </CardText>
+                        <CardText> Ngày làm thêm giờ: {' '}
+                            {staff.overTime}
+                        </CardText>
+
+                    </CardBody>
+                </Card>
+            );
+        }else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
     render(){
         const staffs = this.props.staffs.map((staff)=>{
             if (staff.id<=5){
                 return(
-                    <div key={staff.id} className="col-12 col-md-6 p-4">
-                        <Button className="btn-block" color="info">{staff.name}</Button>
+                    <div key={staff.id} className="col-sm-12 col-md-6 col-lg-4 p-4">
+                        <Button onClick={()=>this.onStaffSelect(staff)} className="btn-block" color="info">{staff.name}</Button>
                     </div>
                 );
             }else{
@@ -25,7 +65,10 @@ class StaffList extends Component{
         return(
             <div className = "container">
                 <div className="row">
-                {staffs}
+                    {staffs}
+                </div>
+                <div className="row">
+                    {this.renderStaff(this.state.selectedStaff,this.props.departments)}
                 </div>
                 
             </div>
@@ -33,19 +76,6 @@ class StaffList extends Component{
         );
     }
 }
-// class StaffList extends Component{
-//     constructor(props){
-//         super(props);
-//         this.state={
-//         }
-//     }
-//     render(){
-//         return(
-//             <div>
-//                 <h1>Hello Word</h1>
-//             </div>
-//         );
-//     }
-// }
+
 
 export default StaffList;
