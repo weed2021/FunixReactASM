071e7a3e7e7344f494fcb8f14d3e6b01257d1783
weed2021 <quanlbfx12493@funix.edu.vn>
2021-10-09 17:48:09 +0700

    Update search staff and filter department in stafflist component

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Card, CardImg, CardHeader,Input } from "reactstrap";
+import { Card, CardImg, CardHeader, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 
 function RenderStaffList({ staffs }) {
     const _staffs = staffs.map((staff) => {
@@ -17,45 +17,95 @@ function RenderStaffList({ staffs }) {
     })
     return (
 
-            <div className="row pt-4" >
-                {_staffs}
-            </div>
+        <div className="row pt-4" >
+            {_staffs}
+        </div>
     );
 }
 
 const StaffList = (props) => {
-    const [searchInput,setSearchInput] = useState("")
-    const newStaffs = props.staffs.filter((staff)=>{
+    // const deparments = props.deparments;
+    // Search input hook
+    const [searchInput, setSearchInput] = useState("")
 
-        if(searchInput===''){
+    // Handle click filter Department
+    const [departmentValue, setDepartmentValue] = useState('')
+
+    
+   
+    const newStaffs = props.staffs.filter(function(staff) {
+
+        if (searchInput === '')  {
             return staff;
-        }else if(staff.name.toLowerCase().includes(searchInput.toLowerCase())){
+
+        }
+        else if (staff.name.toLowerCase().includes(searchInput.toLowerCase())) {
+            
             return staff;
-        }else{
+        } else {
             return null
         }
-
-
     })
-    console.log(searchInput)
+    
+    const staffWithDepartment = props.staffs.filter(function(staff) {
+
+        if (staff.department.name.toLowerCase() === departmentValue)  {
+            return staff;
+
+        }
+        else {
+            return null
+        }
+    })
+    
+    
+
+    // Dropdown hook
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+
+
     return (
         <div style={{ padding: '3vw' }}>
             <div className='row pt-3'>
-                <div className='col-12 col-sm-6 col-md-4 col-lg-3 bg-info p-1 ml-auto' >
-                    <Input onChange={(event) => setSearchInput(event.target.value)} className='input-lg' placeholder="Tìm kiếm nhân viên"  type='text' />
+
+                <div className='col-12 col-sm-6 col-md-8 col-lg-9 p-1 ml-auto' >
+                    <Dropdown className="ml-auto" isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle color='info' caret>
+                            Phòng ban
+                        </DropdownToggle>
+
+                        <DropdownMenu>
+                            <DropdownItem onClick={() => setDepartmentValue('sale')}>Sale</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => setDepartmentValue('hr')}>HR</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => setDepartmentValue('marketing')}>Marketing</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => setDepartmentValue('it')}>IT</DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem onClick={() => setDepartmentValue('finance')}>Finance</DropdownItem>
+
+                        </DropdownMenu>
+                    </Dropdown>
                 </div>
+                <div className='col-12 col-sm-6 col-md-4 col-lg-3 bg-info p-1 ml-auto' >
+                    <Input onFocus={()=> setDepartmentValue('')} onChange={(event) => setSearchInput(event.target.value)} className='input-lg' placeholder="Tìm kiếm nhân viên" type='text' />
+                </div>
+
             </div>
-            
+
             <div className="row pt-5" >
                 <div className='col-12'>
                     <h3 className='text-center font-weight-bold'>DANH SÁCH NHÂN VIÊN</h3>
                     <hr />
                 </div>
             </div>
-            
-            <RenderStaffList staffs={newStaffs} />
+
+            <RenderStaffList staffs={departmentValue===''?newStaffs:staffWithDepartment} />
         </div>
-        
+
     );
 
 };
